4afd9a4ea4daa69a078def385aed8c3ecfcd47cf
tetsuya-azami <zqianjian35@gmail.com>
2025-02-05 09:15:01 +0900

    learning smtp

--- smtp.go
+++ smtp.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/smtp"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Set up authentication information.
+	auth := smtp.PlainAuth("", "user@example.com", "password", "localhost")
+
+	// Connect to the server, authenticate, set the sender and recipient,
+	// and send the email all in one step.
+	to := []string{"recipient@example.net"}
+	msg := []byte("To: recipient@example.net\r\n" +
+		"Subject: discount Gophers!\r\n" +
+		"\r\n" +
+		"This is the email body.\r\n")
+	err := smtp.SendMail("localhost:1025", auth, "sender@example.org", to, msg)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
